9a6e892b5b2001555ed7668c8792901603eac4df
vijithahmbandara <vijithahmbandara@gmail.com>
2023-08-22 21:53:45 +0530

    data send part

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -1,5 +1,69 @@
 import React from "react";
 import './main.css'
+import img from '../../Assets/img(1).jpg'
+const Data = [
+    {
+        id:1,
+        imgSrc:img,
+        destTitle: 'Bora Bora',
+        location:'New Zealand',
+        grade:'CULTURAL RELAX',
+        fees:'$700',
+        description:'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa.'
+    },
+
+    {
+        id:1,
+        imgSrc:img,
+        destTitle: 'Bora Bora',
+        location:'New Zealand',
+        grade:'CULTURAL RELAX',
+        fees:'$700',
+        description:'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa.'
+    },
+
+    {
+        id:1,
+        imgSrc:img,
+        destTitle: 'Bora Bora',
+        location:'New Zealand',
+        grade:'CULTURAL RELAX',
+        fees:'$700',
+        description:'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa.'
+    },
+
+    {
+        id:1,
+        imgSrc:img,
+        destTitle: 'Bora Bora',
+        location:'New Zealand',
+        grade:'CULTURAL RELAX',
+        fees:'$700',
+        description:'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa.'
+    },
+
+    {
+        id:1,
+        imgSrc:img,
+        destTitle: 'Bora Bora',
+        location:'New Zealand',
+        grade:'CULTURAL RELAX',
+        fees:'$700',
+        description:'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa.'
+    },
+
+    {
+        id:1,
+        imgSrc:img,
+        destTitle: 'Bora Bora',
+        location:'New Zealand',
+        grade:'CULTURAL RELAX',
+        fees:'$700',
+        description:'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa.'
+    },
+]
+
+
 const Main = () => {
     return(
         <section className="main container section">
@@ -7,6 +71,26 @@ const Main = () => {
                 <h3 className="title">
                     Most visited destinations
                 </h3>
+            </div>
+
+            <div className="secContent grid">
+                {
+
+                }
+
+                {
+                    Data.map(({id, imgSrc, destTitle, location, grade, fees, description})=>{
+                        return(<div key={id}
+                            className="singleDestination">
+                                {
+                                    <div className="ImageDiv">
+                                        <img src={} alt=""/>
+                                    </div>
+                                }
+                            </div>
+                            )
+                    })
+                }
             </div>
         </section>
     )
