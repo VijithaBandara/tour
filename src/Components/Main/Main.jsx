f4ba7ab41900bdb56b5ae7b8aa188dc4e0c0e2e7
vijithahmbandara <vijithahmbandara@gmail.com>
2023-08-22 21:26:14 +0530

    start main component

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -2,7 +2,13 @@ import React from "react";
 import './main.css'
 const Main = () => {
     return(
-        <div>main</div>
+        <section className="main container section">
+            <div className="secTitle">
+                <h3 className="title">
+                    Most visited destinations
+                </h3>
+            </div>
+        </section>
     )
 }
 
