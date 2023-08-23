2f8dd699ab9a95de5df9c281cada733647808ad4
vijithahmbandara <vijithahmbandara@gmail.com>
2023-08-23 23:46:15 +0530

    creating footer and adding footer.scss

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,8 +1,30 @@
 import React from "react";
 import './Footer.css'
+import video2 from '..//import {FiSend} from 'react-icons/fi'
+
 const Footer = () => {
     return(
-        <div>footer</div>
+        <section className="footer">
+            <div className="videoDiv">
+                <video src={video2} loop autoPlay type="video/mp4"></video>
+            </div>
+
+            <div className="secContent container">
+                <div className="contactDiv flex">
+                    <div className="text">
+                        <small>KEEP IN TOUCH</small>
+                        <h2>Travel with us</h2>
+                    </div>
+
+                    <div className="inputDiv flex">
+                        <input type="text" placeholder='Enter Email Address'/>
+                        <button className='btn flex' type='submit'>
+                            SEND <FiSend className="icon"/>
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </section>
     )
 }
 
