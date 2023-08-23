6f367abdf44cbb531460dd8dc535e59b553ef1c3
vijithahmbandara <vijithahmbandara@gmail.com>
2023-08-24 00:23:22 +0530

    add some part for footer.scss

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import './Footer.css'
-import video2 from '..//import {FiSend} from 'react-icons/fi'
+import video2 from '..//../Assets/video (2).mp4'
+import {FiChevronRight, FiSend} from 'react-icons/fi'
+import { MdOutlineTravelExplore } from "react-icons/md";
+import { AiFillInstagram, AiFillYoutube, AiOutlineTwitter, } from "react-icons/ai";
+import { FaTripadvisor } from "react-icons/fa";
 
 const Footer = () => {
     return(
@@ -21,6 +25,60 @@ const Footer = () => {
                         <button className='btn flex' type='submit'>
                             SEND <FiSend className="icon"/>
                         </button>
+                    </div>
+                </div>
+
+                <div className="footerCard flex">
+                    <div className="footerIntro flex">
+                        <div className="logoDiv">
+                            <a href="#" className="logo flex">
+                                <MdOutlineTravelExplore
+                                className="icon"/>Travel.
+                            </a>
+                        </div>
+
+                        <div className="footerParagraph">
+                            kytdyktjhkhfutilyiy
+                            dfbfbzdfbzdfbzdfbzfb
+                            sfgzsfbzdfbzdfbzdfb
+                            dfbzdfbxbfxhmghxd
+                            dfhzdfhzfdhzdfhzdhzdfh
+                        </div>
+
+                        <div className="footerSocials">
+                            <AiOutlineTwitter className="icon"/>
+                            <AiFillYoutube className="icon"/>
+                            <AiFillInstagram className="icon"/>
+                            <FaTripadvisor className="icon"/>
+                        </div>
+                    </div>
+                    <div className="footerLinks grid">
+                        {/* */}
+                        <div className="linkGroup">
+                            <span className="groupTitle">
+                                OUR AGENCY
+                            </span>
+                            <li className="footerList flex">
+                                <FiChevronRight className="icon"/>
+                                Services
+                            </li>
+                            <li className="footerList flex">
+                                <FiChevronRight className="icon"/>
+                                Insurance
+                            </li>
+                            <li className="footerList flex">
+                                <FiChevronRight className="icon"/>
+                                Agency
+                            </li>
+                            <li className="footerList flex">
+                                <FiChevronRight className="icon"/>
+                                Tourism
+                            </li>
+                            <li className="footerList flex">
+                                <FiChevronRight className="icon"/>
+                                Payment
+                            </li>
+                        </div>
                     </div>
                 </div>
             </div>
